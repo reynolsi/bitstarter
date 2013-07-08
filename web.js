28ef49769bd85e1fc55a50be4e1d47409501e6f9
Ubuntu <ubuntu@ip-172-31-16-112.us-west-2.compute.internal>
2013-07-08 06:15:20 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,8 +2,23 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require("fs");
+
+var FILENAME_DEFAULT = "./index.html";
+
+var readFile = function(fileName) {
+  if(fileName.length > 0) {
+    fs.exists(fileName, function(exists){
+      if(exists)fs.readFile(fileName, "utf-8", function(error, data) {
+        console.log(data);
+      });
+      else console.log("file not found");
+    });
+  } else { /* do nothing */ }
+};
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  readFile(fileName);
 });
 
 var port = process.env.PORT || 5000;
